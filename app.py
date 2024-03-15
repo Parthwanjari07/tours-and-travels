951da15c3b413e6d078c443f8379bbb2ccc07111
kashewknutt <141485474+kashewknutt@users.noreply.github.com>
2024-03-15 19:06:29 +0530

    added multiple recipients key

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ def places():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    recipients = [
+        {'access_key': 'c7b61e5e-e90c-4550-966d-09501ca9a413'},
+        {'access_key': 'f7a528e9-d257-4d81-8583-af838851cacc'}
+    ]
+    return render_template('contact.html', recipients=recipients)
 
 @app.route('/contact2.html')
 def contact2():
